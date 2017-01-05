1612d44305a8d3205a692123d891aed8bd66a41d
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-04 16:23:10 -0800

    try and except

--- calculator.py
+++ calculator.py
@@ -15,19 +15,22 @@ while True:
     if token_input[0] == 'q' or token_input[0] == 'quit':
         break
     else:
-        if token_input[0] == "+" or token_input[0] == "add":
-            print add(int(token_input[1]), int(token_input[2]))
-        if token_input[0] == "-" or token_input[0] == "subtract":
-            print subtract(int(token_input[1]), int(token_input[2]))
-        if token_input[0] == "*" or token_input[0] == "multiply":
-            print multiply(int(token_input[1]), int(token_input[2]))
-        if token_input[0] == "/" or token_input[0] == "divide":
-            print divide(int(token_input[1]), int(token_input[2]))
-        if token_input[0] == "square":
-            print square(int(token_input[1]))
-        if token_input[0] == "cube":
-            print cube(int(token_input[1]))
-        if token_input[0] == "pow":
-            print power(int(token_input[1]), int(token_input[2]))
-        if token_input[0] == "mod":
-            print mod(int(token_input[1]), int(token_input[2]))
+        try:
+            if token_input[0] == "+" or token_input[0] == "add":
+                print add(int(token_input[1]), int(token_input[2]))
+            if token_input[0] == "-" or token_input[0] == "subtract":
+                print subtract(int(token_input[1]), int(token_input[2]))
+            if token_input[0] == "*" or token_input[0] == "multiply":
+                print multiply(int(token_input[1]), int(token_input[2]))
+            if token_input[0] == "/" or token_input[0] == "divide":
+                print divide(int(token_input[1]), int(token_input[2]))
+            if token_input[0] == "square":
+                print square(int(token_input[1]))
+            if token_input[0] == "cube":
+                print cube(int(token_input[1]))
+            if token_input[0] == "pow":
+                print power(int(token_input[1]), int(token_input[2]))
+            if token_input[0] == "mod":
+                print mod(int(token_input[1]), int(token_input[2]))
+        except:
+            print "Please enter valid integer."
