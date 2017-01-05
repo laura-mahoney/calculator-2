1bd1daa7d947bbeda5c61755aef4e0dd4ae508e7
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-04 16:06:32 -0800

    added arithmetic functions

--- calculator.py
+++ calculator.py
@@ -14,5 +14,20 @@ while True:
     token_input = user_input.split(" ")
     if token_input[0] == 'q' or token_input[0] == 'quit':
         break
-    # else: 
-    
+    else:
+        if token_input[0] == "+":
+            add(token_input[1], token_input[2])
+        if token_input[0] == "-":
+            subtract(token_input[1], token_input[2])
+        if token_input[0] == "*":
+            multiply(token_input[1], token_input[2])
+        if token_input[0] == "/":
+            divide(token_input[1], token_input[2])
+        if token_input[0] == "square":
+            square(token_input[1])
+        if token_input[0] == "cube":
+            cube(token_input[1])
+        if token_input[0] == "pow":
+            power(token_input[1], token_input[2])
+        if token_input[0] == "mod":
+            mod(token_input[1], token_input[2])
